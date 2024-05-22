b87530fd00a5539fe84057054ecfa4bc925b21cd
DataGeek404 <techjayprivate@gmailcom>
2024-05-22 10:52:09 +0300

    Initial commit

---
+++ Day_02_Variables/hello.py
@@ -0,0 +1,10 @@
+"""
+
+Built in functions
+In Python we have lots of built-in functions. 
+Built-in functions are globally available for your use that mean you can make use of the built-in functions without importing or configuring.
+Some of the most commonly used Python built-in functions are the following: 
+print(), len(), type(), int(), float(), str(), input(), list(), dict(), min(), max(), sum(), sorted(), open(), file(), help(), 
+and dir(). In the following table you will see an exhaustive list of Python built-in functions taken from python documentation.
+"""
+
